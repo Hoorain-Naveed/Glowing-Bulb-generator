e83e22c45fe071996bf5c55ab60d83b314cb75bf
Hoorain Naveed <125212055+Hoorain-Naveed@users.noreply.github.com>
2023-12-31 10:55:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+setInterval(async function() {
+    let bulb=  document.querySelector('#bulb').classList.toggle('bulb')
+  console.log(bulb);
+  }, 500);
+
+  // setTimeout 1 times chlta ha
+  // setInterval repetadly chlta ha jb tk hum usa off na krdyn
+
